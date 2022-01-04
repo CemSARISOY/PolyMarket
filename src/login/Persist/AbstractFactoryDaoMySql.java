d4deab049fb4da4e6960a182021d6f5b0ef1587b
CemSARISOY <cem.sarisoy@etu.umontpellier.fr>
2022-01-04 11:59:13 +0100

    Revert "bug git"
    This reverts commit ba058609ff516bfd4fd44525fc054f45cfede7b5.

--- src/login/Persist/AbstractFactoryDaoMySql.java
+++ src/login/Persist/AbstractFactoryDaoMySql.java
@@ -1,25 +1,36 @@
 package Persist;
 
-import java.sql.*;
+import java.sql.Connection;
 
-public class AbstractFactoryDaoMySQL extends AbstractFactoryDao {
+/**
+* @generated
+*/
+public class AbstractFactoryDaoMySql extends AbstractFactoryDao {
+    
+    
+    
+    
 
-    Connection con;
-
-    public UserDao createUserDao() {
-        return new UserDaoMySQL(this);
+    //                          Operations                                  
+    
+    /**
+    * @generated
+    */
+    public AuctionDaoMySql createAuctionDao() {
+        return null;
+        //TODO
     }
 
-    public Connection getConnection() {
-        try {
-            if (con == null)
-                con = DriverManager.getConnection("jdbc:mysql://eu01-db.cus.mc-panel.net/db_442584", "db_442584",
-                        "9bfc0fd115");
-            return con;
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    @Override
+    public UserDao createUserDao() {
+        // TODO Auto-generated method stub
         return null;
     }
 
+    @Override
+    public Connection getConnection() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
 }
